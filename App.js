88c74202574ae2833fb97e7fd9e91c1ccf1c282e
piedpipr <43669876+piedpipr@users.noreply.github.com>
2021-07-15 10:43:07 +0600

    designed login page in main App

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, Text, Button, Alert, View, Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Rancho_400Regular } from '@expo-google-fonts/rancho';
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Rancho_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.title}>
+        <Text style={{ fontFamily: 'Rancho_400Regular', fontSize: 80, color: 'red' }}>GradMate</Text>
+        <Image 
+          source={require('./assets/home.gif')}  
+          style={{width: 350, height: 350 }}/>
+        <Text style={{ fontWeight: 'bold', fontSize: 18, color: '#219ebc' }}>Welcome to Gradmate</Text>
+      </View>
+      <Separator />
+      <View style={styles.buttons}>
+        <Button
+        marginRight='190px'
+        color='#219ebc'
+        title="Login with Google"
+        onPress={() => Alert.alert('Simple Button pressed')}/>
+        <Separator />
+        <Button
+        color='#fb8500'
+        title="Continue Without Login"
+        onPress={() => Alert.alert('Simple Button pressed')}/>
+      </View>
+      <Separator />
+      <View style={{ marginHorizontal: 25, marginBottom: -100 }} ><Text style={{ textAlign: 'center',fontSize: 13, color: '#219ebc' }}>Please login with Gmail to save your progress.{ "\n" }Otherwise select continue without log in to use the app</Text></View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS==="android" ? StatusBar.currentHeight : 0,
+  },
+  title: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttons: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+  },
+  separator: {
+    marginVertical: 10,
   },
 });
