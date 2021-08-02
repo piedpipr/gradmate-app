a86f1ef08645ce4cddacc6c20a56abec7190ec5d
piedpipr <43669876+piedpipr@users.noreply.github.com>
2021-08-02 06:27:03 +0600

    offline user data management + pie chart working

--- screens/dashboard.js
+++ screens/dashboard.js
@@ -12,173 +12,259 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import {PieChart} from 'react-native-chart-kit';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import {Card} from 'react-native-shadow-cards';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firestore from '@react-native-firebase/firestore';
 
-const Separator = () => <View style={styles.separator} />;
 const screenWidth = Dimensions.get('window').width / 1.1;
 
-export default function Dashboard() {
-  const [isUser, setUser] = useState(null);
-
-  const CurrentUser = () => {
-    if (isUser == null) {
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUser: null,
+      isUserData: null,
+    }; // ISUSER = LOGGED IN USER DATA FROM AUTH, ISUSERDATA = USER APP USAGE DATA
+  }
+  //////////////////////////////////////////////////////////////////////////////////////
+  CurrentUser = () => {
+    if (this.state.isUser == null) {
       const valuePromise = AsyncStorage.getItem('currentUser');
       valuePromise.then(value => {
         let val = JSON.parse(value);
-        setUser(val);
-        console.log(val);
+        console.log('val.uid');
+        console.log(val.uid);
+        this.setState({isUser: val});
       });
     }
+  }; // GET LOGGED IN USER DETAILS DOCUMENTSNAPSHOT SAVED IN ASYNCH FROM APP.JS
+
+  ///////////////////////////////////////////////////////////////////////////////
+  UserDATA = () => {
+    if (this.state.isUser && !this.state.isUserData) {
+      if (!this.state.isUser.isAnonymous) {
+        let GetUserData = async () => {
+          const events = await firestore()
+            .collection('testusers') //CHANGE TO ORIGINSL USER COLLECTION
+            .doc(this.state.isUser.uid);
+          events
+            .get()
+            .then(documentSnapshot => {
+              console.log(documentSnapshot.data());
+              if (!this.state.isUserData) {
+                console.log('Howdy Man');
+                AsyncStorage.setItem(
+                  'userOrigin',
+                  JSON.stringify(documentSnapshot.data()),
+                ); // SAVE DATA IN ASYNC FOR ORIGIN DATA
+                console.log('FETCHED ORIGIN USER DATA');
+                AsyncStorage.getItem('userLocal').then(item => {
+                  if (!item) {
+                    AsyncStorage.setItem(
+                      'userLocal',
+                      JSON.stringify(documentSnapshot.data()),
+                    );
+                    console.log('CREATED AND SAVED LOCAL USER DATA');
+                  }
+                }); // SAVE DATA IN ASYNC FOR 1st TIME USE IN LOCAL (IF LOCAL DOESN'T EXIST YET)
+                this.setState({isUserData: documentSnapshot.data()});
+              }
+            })
+            .catch(error => {
+              console.log(error);
+              console.log('Error Showed');
+            });
+        };
+        GetUserData();
+      }
+    }
+  }; //CHECKS IF THE LOGGED IN USER IS NON ANONYMOUS, THEN SAVE DATA FROM DB TO LOCAL AND UPDATE IN EACH RENDER
+
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  ChartDATA = () => {
+    if (this.state.isUserData) {
+      if (!this.state.isUser.isAnonymous) {
+        let userdata = this.state.isUserData;
+        let Learned = userdata.learned.split(',');
+        let Learning = userdata.learning.split(',');
+        let Arr = [Learning, Learned];
+        return Arr;
+      }
+    } else {
+      let Arr = [['Example Set'], ['Example Set']];
+      return Arr;
+    }
   };
+  /////////////////////////////////////////////////////////////////////////////////////////
+  componentDidMount() {
+    this.CurrentUser();
+  }
 
-  useEffect(() => {
-    CurrentUser();
-  }, []);
-
-  console.log(isUser);
-
-  if (isUser) {
-    if (isUser.isAnonymous) {
-      return (
-        <SafeAreaView style={styles.container}>
-          <View style={styles.title}>
+  render() {
+    this.UserDATA();
+    let PieData = this.ChartDATA();
+    console.log(this.state.isUser);
+    console.log(this.state.isUserData);
+    if (this.state.isUser) {
+      if (this.state.isUser.isAnonymous) {
+        return (
+          <SafeAreaView style={styles.container}>
+            <View style={styles.title}>
+              <Text
+                style={{
+                  fontFamily: 'Rancho-Regular',
+                  fontSize: 80,
+                  color: 'white',
+                  paddingBottom: 30,
+                  paddingTop: 40,
+                }}>
+                Dashboard
+              </Text>
+              <Card
+                cornerRadius={15}
+                style={{
+                  elevation: 0,
+                  backgroundColor: 'white',
+                  ...styles.containercard,
+                }}>
+                <Image
+                  source={require('../assets/icons/user.png')}
+                  style={{width: 140, height: 140, borderRadius: 70}}
+                />
+                <Text
+                  style={{
+                    fontSize: 30,
+                    color: '#3395ff',
+                    paddingTop: 10,
+                    paddingBottom: 0,
+                    fontWeight: 'bold',
+                  }}>
+                  Anonymous
+                </Text>
+              </Card>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 16,
+                  color: 'white',
+                  paddingTop: 60,
+                  paddingHorizontal: 25,
+                }}>
+                Please login with Google to access your dashboard. In the
+                current build we are unable to provide dashboard functionality
+                to the anonymous user. {'\n'}
+                {'\n'}To login with Google clear the app data from Android
+                setting and re-run the app. Current data will be erased.
+              </Text>
+            </View>
+          </SafeAreaView>
+        );
+      } else {
+        ///////////////////////////////////////////////////////////
+        const data = [
+          {
+            name: 'Learning',
+            population: PieData[0].length,
+            color: '#febe29',
+            legendFontColor: 'white',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Learned',
+            population: PieData[1].length,
+            color: '#f75689',
+            legendFontColor: 'white',
+            legendFontSize: 15,
+          },
+        ];
+        ////////////////////////////////////////////////////////////
+        const userphoto = this.state.isUser.photoURL;
+        return (
+          <SafeAreaView style={styles.container}>
             <Text
               style={{
                 fontFamily: 'Rancho-Regular',
                 fontSize: 80,
+                textAlign: 'center',
                 color: 'white',
-                paddingBottom: 30,
-                paddingTop: 40,
+                paddingBottom: 0,
+                marginBottom: 0,
+                paddingTop: 30,
               }}>
               Dashboard
             </Text>
-            <Card
-              cornerRadius={15}
-              style={{
-                elevation: 0,
-                backgroundColor: 'white',
-                ...styles.containercard,
-              }}>
-              <Image
-                source={require('../assets/icons/user.png')}
-                style={{width: 140, height: 140, borderRadius: 70}}
-              />
-              <Text
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+              <Card
+                cornerRadius={15}
                 style={{
-                  fontSize: 30,
-                  color: '#3395ff',
-                  paddingTop: 10,
-                  paddingBottom: 0,
-                  fontWeight: 'bold',
+                  elevation: 0,
+                  paddingTop: 30,
+                  paddingBottom: 30,
+                  marginTop: 20,
+                  backgroundColor: 'rgba(52, 52, 52, 0.1)',
+                  ...styles.containercard,
                 }}>
-                Anonymous
-              </Text>
-            </Card>
-            <Text
-              style={{
-                textAlign: 'center',
-                fontSize: 16,
-                color: 'white',
-                paddingTop: 60,
-                paddingHorizontal: 25,
-              }}>
-              Please login with Google to access your dashboard. In the current
-              build we are unable to provide dashboard functionality to the
-              anonymous user. {'\n'}
-              {'\n'}To login with Google clear the app data from Android setting
-              and re-run the app. Current data will be erased.
-            </Text>
-          </View>
-        </SafeAreaView>
-      );
+                <View>
+                  {userphoto == null ? (
+                    <Image
+                      source={require('../assets/icons/user.png')}
+                      style={{width: 140, height: 140, borderRadius: 70}}
+                    />
+                  ) : (
+                    <Image
+                      source={{
+                        uri: userphoto,
+                      }}
+                      style={{width: 140, height: 140, borderRadius: 70}}
+                    />
+                  )}
+                </View>
+                <Text
+                  style={{
+                    fontSize: 30,
+                    color: 'white',
+                    paddingTop: 10,
+                    paddingBottom: 0,
+                    fontWeight: 'bold',
+                  }}>
+                  {this.state.isUser.displayName}
+                </Text>
+              </Card>
+              <Card
+                cornerRadius={20}
+                style={{
+                  marginTop: 10,
+                  paddingBottom: 80,
+                  elevation: 0,
+                  backgroundColor: 'rgba(52, 52, 52, 0.1)',
+                }}>
+                <Text style={styles.heading}>STATS</Text>
+                <PieChart
+                  data={data}
+                  width={screenWidth}
+                  height={220}
+                  chartConfig={chartConfig}
+                  accessor={'population'}
+                  backgroundColor={'transparent'}
+                  paddingLeft={'0'}
+                  center={[10, -10]}
+                  absolute
+                />
+                <Text style={styles.heading}>LEARNING</Text>
+                <Text style={styles.heading}>LEARNED</Text>
+              </Card>
+            </ScrollView>
+          </SafeAreaView>
+        );
+      }
     } else {
-      const userphoto = isUser.photoURL;
       return (
         <SafeAreaView style={styles.container}>
-          <Text
-            style={{
-              fontFamily: 'Rancho-Regular',
-              fontSize: 80,
-              textAlign: 'center',
-              color: 'white',
-              paddingBottom: 0,
-              marginBottom: 0,
-              paddingTop: 30,
-            }}>
-            Dashboard
-          </Text>
-
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <Card
-              cornerRadius={15}
-              style={{
-                elevation: 0,
-                paddingTop: 30,
-                paddingBottom: 30,
-                marginTop: 20,
-                backgroundColor: 'rgba(52, 52, 52, 0.1)',
-                ...styles.containercard,
-              }}>
-              <View>
-                {userphoto == null ? (
-                  <Image
-                    source={require('../assets/icons/user.png')}
-                    style={{width: 140, height: 140, borderRadius: 70}}
-                  />
-                ) : (
-                  <Image
-                    source={{
-                      uri: userphoto,
-                    }}
-                    style={{width: 140, height: 140, borderRadius: 70}}
-                  />
-                )}
-              </View>
-              <Text
-                style={{
-                  fontSize: 30,
-                  color: 'white',
-                  paddingTop: 10,
-                  paddingBottom: 0,
-                  fontWeight: 'bold',
-                }}>
-                {isUser.displayName}
-              </Text>
-            </Card>
-            <Card
-              cornerRadius={20}
-              style={{
-                marginTop: 10,
-                paddingBottom: 80,
-                elevation: 0,
-                backgroundColor: 'rgba(52, 52, 52, 0.1)',
-              }}>
-              <Text style={styles.heading}>STATS</Text>
-              <PieChart
-                data={data}
-                width={screenWidth}
-                height={220}
-                chartConfig={chartConfig}
-                accessor={'population'}
-                backgroundColor={'transparent'}
-                paddingLeft={'0'}
-                center={[10, -10]}
-                absolute
-              />
-              <Text style={styles.heading}>COMPLETED</Text>
-              <Text style={styles.heading}>INCOMPLETE</Text>
-            </Card>
-          </ScrollView>
+          <ActivityIndicator />
         </SafeAreaView>
       );
     }
-  } else {
-    return (
-      <SafeAreaView style={styles.container}>
-        <ActivityIndicator />
-      </SafeAreaView>
-    );
   }
 }
 
@@ -212,9 +298,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-start',
   },
-  separator: {
-    marginVertical: 10,
-  },
 });
 
 const chartConfig = {
@@ -227,27 +310,3 @@ const chartConfig = {
   barPercentage: 0.5,
   useShadowColorFromDataset: false, // optional
 };
-
-const data = [
-  {
-    name: 'Completed',
-    population: 280,
-    color: '#febe29',
-    legendFontColor: 'white',
-    legendFontSize: 15,
-  },
-  {
-    name: 'Uncompleted',
-    population: 900,
-    color: '#f75689',
-    legendFontColor: 'white',
-    legendFontSize: 15,
-  },
-  {
-    name: 'Unopened',
-    population: 1500,
-    color: 'white',
-    legendFontColor: 'white',
-    legendFontSize: 15,
-  },
-];
